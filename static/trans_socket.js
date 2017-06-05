0adb8b592f32d1c8de716adb27418269103692e8
WoIstOben <woistoben@gmail.com>
2017-06-05 17:07:19 +0200

    Update trans_socket.js

--- static/trans_socket.js
+++ static/trans_socket.js
@@ -1,7 +1,6 @@
 // Send Data
 Send_Socket = function(socket, name, data) {
     try {
-        //for(var i = 0;i < 1000000;i++) ;
         socket.emit(name, data);
     } catch(e) {
         console.log(e);
